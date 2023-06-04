3c0a9f1e4dcac1f7d78e0e7c17658223e795e019
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-06-04 23:32:47 +0800

    Update style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,20 +39,30 @@ input[type="submit"] {
 	padding: 6px 8px;
 }
 
-.query-para > * {
+form {
+	margin: 1em 0;
+}
+
+form > * {
 	vertical-align: middle;
+}
+
+footer {
+	font-size: 90%;
 }
 </style>
 
 <div class="page">
 	<h1>漢字音典</h1>
 
-	<p class="query-para">
+	<form on:submit={handleSubmit}>
 		<input type="text" bind:value={字頭們}>
-		<input type="submit" on:click={handleSubmit} value="查詢">
-	</p>
+		<input type="submit" value="查詢">
+	</form>
 
 	<DataTable results={results}/>
 
-	<p>網站作者：<a href="https://github.com/nk2028/hdqt">nk2028</a> - 資料來源：漢字音典（眾專家）</p>
+	<footer>
+		<p>網站作者：<a href="https://github.com/nk2028/hdqt">nk2028</a> - 資料來源：漢字音典（眾專家）</p>
+	</footer>
 </div>
