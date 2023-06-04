f6c1159ef3b94afbff40eef92a04912038db086a
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-06-04 21:28:04 +0800

    First commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,58 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	import DataTable from './DataTable.svelte';
+
+	let 字頭們 = '';
+	let results = [] as string[][];
+
+	const fetchData = async () => {
+		if (字頭們.length) {
+			const response = await fetch(`https://hdqt.nk2028.shn.hk/query?string=${字頭們}`);
+			results = await response.json() as string[][];
+		}
+	};
+
+	const handleSubmit = () => {
+		fetchData();
+	};
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+.page {
+	text-align: center;
+}
 
-	h1 {
-		width: 100%;
-	}
+input {
+	background-color: inherit;
+	color: inherit;
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+input[type="text"] {
+	border: 1px solid;
+	border-radius: 3px;
+	padding: 6px 8px;
+}
+
+input[type="submit"] {
+	border: 1px solid;
+	border-radius: 5px;
+	padding: 6px 8px;
+}
+
+.query-para > * {
+	vertical-align: middle;
+}
 </style>
+
+<div class="page">
+	<h1>漢字音典</h1>
+
+	<p class="query-para">
+		<input type="text" bind:value={字頭們}>
+		<input type="submit" on:click={handleSubmit} value="查詢">
+	</p>
+
+	<DataTable results={results}/>
+
+	<p>網站作者：<a href="https://github.com/nk2028">nk2028</a> - 資料來源：漢字音典（眾專家）</p>
+</div>
---
+++ src/routes/DataTable.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	export let results: string[][];
+</script>
+
+<style>
+table {
+	border-collapse: collapse;
+	margin: 0 auto;
+	overflow-x: auto;
+}
+
+th, td {
+	border: 1px solid;
+	padding: 4px 6px;
+}
+
+td:first-child {
+	white-space: nowrap;
+}
+
+td:not(:first-child) {
+	width: 6em;
+	word-break: break-all;
+}
+
+.name-tag {
+	border-radius: 10px;
+	color: white;
+	font-size: 0.8em;
+	padding: 3px 5px;
+}
+
+.text {
+	font-size: 90%;
+}
+</style>
+
+{#if results.length}
+	<table>
+		<thead>
+			<tr>
+				{#each results[0] as 字頭}
+					<th>{字頭}</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each results.slice(1) as [簡稱, 分區, 顏色, ...字音們]}
+				<tr>
+					<td><span class="name-tag" style="background-color: {顏色};" title="{分區}">{簡稱}</span></td>
+					{#each 字音們 as 字音}
+						<td lang="zh-x-fonipa" class="text">{字音}</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+{/if}
